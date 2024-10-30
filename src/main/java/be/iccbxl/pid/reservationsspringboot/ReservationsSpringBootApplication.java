89dffd004ea51b881fbe9af60b46626205f5be7d
Josiane <kajosine@gmail.com>
2024-10-30 23:46:36 +0100

    After framework installation

--- src/main/java/be/iccbxl/pid/reservationsspringboot/ReservationsSpringBootApplication.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/ReservationsSpringBootApplication.java
@@ -1,13 +1,39 @@
-package be.iccbxl.pid.reservationsspringboot;
+package be.iccbxl.pid.reservationsspringboot.service;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
-public class ReservationsSpringBootApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReservationsSpringBootApplication.class, args);
+@Service
+public class ArtistService {
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    public List<Artist> getAllArtists() {
+        List<Artist> artists = new ArrayList<>();
+
+        artistRepository.findAll().forEach(artists::add);
+
+        return artists;
     }
+
+    public Artist getArtist(long id) {
+        return artistRepository.findById(id);
+    }
+
+    public void addArtist(Artist artist) {
+        artistRepository.save(artist);
+    }
+
+    public void updateArtist(long id, Artist artist) {
+        artistRepository.save(artist);
+    }
+
+    public void deleteArtist(long id) {
+        artistRepository.deleteById(id);
+    }
+
 }
 
