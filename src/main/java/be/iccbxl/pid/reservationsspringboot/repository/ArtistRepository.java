9eba84b55526139c8071f7c88f5aec127097e646
Josiane <kajosine@gmail.com>
2024-11-05 21:47:54 +0100

    After framework installation

---
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/repository/ArtistRepository.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.reservationsspringboot.repository;
+
+import be.iccbxl.pid.reservationsspringboot.model.Artist;
+
+public class ArtistRepository {
+    public Iterable<Object> findAll() {
+        return null;
+    }
+
+    public Artist findById(long id) {
+        return null;
+    }
+}
