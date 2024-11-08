463f87896fc3e8a834c654e99cb7702ac71eca67
Josiane <kajosine@gmail.com>
2024-11-08 17:22:05 +0100

    After framework installation

--- src/main/java/be/iccbxl/pid/reservationsspringboot/repository/ArtistRepository.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/repository/ArtistRepository.java
@@ -1,13 +1,9 @@
-package be.iccbxl.pid.reservationsspringboot.repository;
-
-import be.iccbxl.pid.reservationsspringboot.model.Artist;
+package be.iccbxl.pid.reservationsSpringBoot.model;
 
 public class ArtistRepository {
-    public Iterable<Object> findAll() {
-        return null;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+    public interface ArtistRepository extends CrudRepository<Artist, Long> {
+        List<Artist> findByLastname(String lastname);
+        Artist findById(long id);
     }
-
-    public Artist findById(long id) {
-        return null;
-    }
-}
